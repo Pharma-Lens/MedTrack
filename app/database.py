714bca7e88d349457961cfaa1178935bab3a13d6
Keith Ndiema K <veritasndiema@gmail.com>
2026-06-20 17:17:07 +0300

    modified database.py

--- app/database.py
+++ app/database.py
@@ -1,10 +1,15 @@
-"""SQLAlchemy engine and session setup."""
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./medtrack.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./medtrack.db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {},
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
