7a407371e85d1eed47a1313264fd195995b79db5
Keith Ndiema K <veritasndiema@gmail.com>
2026-06-20 13:44:15 +0300

    initial database.py

---
+++ app/database.py
@@ -0,0 +1,17 @@
+"""SQLAlchemy engine and session setup."""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "sqlite:///./medtrack.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
